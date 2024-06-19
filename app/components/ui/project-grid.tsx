a5f6c02e05012af28bad262fe416d4897b980a93
Anurag Gaddamwar <gaddamwaranurag@gmail.com>
2024-06-19 18:18:26 +0530

    Added projects

--- app/components/ui/project-grid.tsx
+++ app/components/ui/project-grid.tsx
@@ -1,4 +1,7 @@
 import { cn } from "@/app/utils/cn";
+import React, { useState, useEffect, useRef } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export const BentoGrid = ({
   className,
@@ -25,32 +28,103 @@ export const BentoGridItem = ({
   description,
   header,
   icon,
+  detailedDescription,
+  image,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
+  detailedDescription?: string | React.ReactNode;
+  image?: string;
 }) => {
-  return (
-    <div
-      className={cn(
-        "row-span-1 ml-10 mr-8 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-black border border-transparent justify-between flex flex-col space-y-4",
-        className
-      )}
-    >
-      
+  const [isOpen, setIsOpen] = useState(false);
+  const popupRef = useRef<HTMLDivElement>(null);
+  const triggerRef = useRef<HTMLDivElement>(null); 
 
-      {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
-        <div className="font-sans font-bold text-neutral-200  mb-2 mt-2">
-          {title}
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.body.style.overflow = isOpen ? 'hidden' : 'auto'; 
+
+    if (isOpen) {
+      popupRef.current?.focus(); // Focus on the popup when it opens
+    } else {
+      triggerRef.current?.focus(); // Return focus to the trigger when closing
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
+
+
+  return (
+    <div>
+      <div
+      ref={triggerRef}
+        onClick={togglePopup}
+        className={cn(
+          "cursor-pointer row-span-1 ml-10 mr-8 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 border-white/[0.1] bg-black border justify-between flex flex-col space-y-4",
+          className
+        )}
+      >
+        {header}
+        <div className="group-hover/bento:translate-x-2 transition duration-200 flex items-center justify-between">
+          <div className="flex items-center">
+            {icon}
+            <div className="font-mono font-bold text-neutral-200 mb-2 ml-2">
+              {title}
+            </div>
+          </div>
+          {image && <img src={image} alt={`${title} logo`} className="w-10 h-10 rounded-full" />}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div className="font-mono ml-2 font-normal text-neutral-600 text-xs dark:text-neutral-300">
           {description}
         </div>
       </div>
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+          >
+            <motion.div
+              ref={popupRef}
+              layout
+              className="bg-white dark:bg-black rounded-lg p-8 w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 relative"
+            >
+              <button
+                onClick={togglePopup}
+                className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 dark:hover:text-gray-300"
+              >
+                <FaTimes />
+              </button>
+              <div className="flex flex-col items-center">
+                {image && <img src={image} alt={`${title} image`} className="mb-4 rounded-lg w-20" />}
+                <h2 className="text-2xl font-bold mb-4">{title}</h2>
+                <div className="text-sm dark:text-neutral-300">{detailedDescription}</div>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -21,35 +21,74 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen w-screen flex-col items-center relative">
-
       <div className="rounded-xl w-full sm:text-5xl tracking-widest font-mono text-4xl sm:h-32 z-10">
         <AuroraBackground>ANURAG GADDAMWAR</AuroraBackground>
       </div>
       {/* <BackgroundBeams className="absolute top-0 w-full h-full mb-40" /> */}
-
       <TextRevealCard text="Web Developer" revealText="Web Designer" />
       <div className="mt-8">
         <TracingBeam {...{ svgHeight: aboutHeight }}>
           <About />
           <Skills />
-          <h1 className="text-sky-300 mt-10 text-2xl md:text-3xl  mx-10 mb-10">Projects</h1>
-
+          <h1 className="text-sky-300 font-mono tracking-widest mt-10 text-2xl md:text-3xl mx-10 mb-10">Projects</h1>
           <BentoGrid>
             <BentoGridItem 
               title="Cypher AI" 
               description="AI-Powered Interview and Resume Preparation Platform" 
-              icon={<i className="fas fa-robot"></i>} // Add your preferred icon here
+              detailedDescription={
+                <>
+                  <ul className="list-disc text-sm text-justify">
+                    <li>
+                      Implemented an interactive chatbot (both text-based and voice-based) utilizing the Gemini API to provide 
+                      personalized responses and guidance for technical and interpersonal interview questions.
+                    </li>
+                    <li>
+                      Developed a resume analysis feature that accepts job roles and resumes (in PDF or image format) from users. 
+                      This feature extracts text from uploaded documents and uses the Gemini API to evaluate resumes against specific 
+                      job roles, offering detailed feedback including ATS (Applicant Tracking System) compatibility scores, structure 
+                      analysis, and content relevance scores.
+                    </li>
+                    <li>
+                      <span className="font-semibold">Outcome:</span> Created a comprehensive tool that empowers job seekers with tailored interview practice 
+                      and resume enhancement suggestions, significantly increasing their chances of success in the job application process.
+                    </li>
+                    <li>
+                      <span className="font-semibold">Tech Stack:</span> Next.js, React, react-markdown, WebKitSpeech API, OCR, Express.js, Node.js, MongoDB, Gemini API
+                    </li>
+                  </ul>
+                </>
+              }
+              icon={<i className="fas fa-robot"></i>}
+              image="cypher.png"
             />
-            <BentoGridItem 
-              title="DU GUIDEBOOK" 
-              description="A Delhi University Magazine" 
-              icon={<i className="fas fa-book"></i>} // Add your preferred icon here
-            />
-
             <BentoGridItem 
               title="EVOLVE" 
               description="YouTube Optimizer" 
-              icon={<i className="fas fa-video"></i>} // Add your preferred icon here
+              detailedDescription={
+                <>
+                  <ul className="list-disc text-sm text-justify">
+                    <li>
+                      Developed a web application designed to streamline content creation by summarizing audience feedback 
+                      from YouTube comments and providing data-driven recommendations to creators.
+                    </li>
+                    <li>
+                      Integrated the Gemini API and YouTube Data API to collect and evaluate comments, highlighting key insights 
+                      and understanding audience perceptions. Features were developed to identify and filter out spam and abusive 
+                      comments, ensuring that creators receive only relevant and constructive feedback.
+                    </li>
+                    <li>
+                      <span className="font-semibold">Outcome:</span> Created a powerful tool that enables creators to understand audience sentiment, identify 
+                      areas for improvement, receive content recommendations, and maintain a positive comment section. This 
+                      enhances content quality and viewer engagement.
+                    </li>
+                    <li>
+                      <span className="font-semibold">Tech Stack:</span> Next.js, MongoDB, Express.js, Node.js, React.js, Gemini API, YouTube Data API
+                    </li>
+                  </ul>
+                </>
+              }
+              icon={<i className="fas fa-video"></i>}
+              image="evolve.png"
             />
           </BentoGrid>
         </TracingBeam>
